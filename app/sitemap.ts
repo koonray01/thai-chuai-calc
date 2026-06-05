9560a5e1f5a6151c921058fd10b9008a048a206b
AK <keawpilarom@gmail.com>
2026-06-05 17:57:29 +0700

    Simplify sitemap for Google Search Console

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,7 +6,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: siteUrl,
-      lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
     },
